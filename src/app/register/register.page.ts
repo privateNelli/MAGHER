b91052971178997fd851d899c528d62d1439f0b5
Miguelasg1999 <miguelasg1999@gmail.com>
2024-09-13 02:43:25 -0300

    Navigation, routerLinks and defects

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  usuario = {
+
+      nombre:"",
+      apellido:"",
+      correo:"",
+      rut:"",
+      telefono:"",
+      contrasena:"",
+      contrasena2:"",
+
+  }
+
+  constructor(private nCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+
+  sendToProfile(){
+    
+    let nExtras: NavigationExtras = {
+      queryParams:{
+        nombre: this.usuario.nombre,
+        apellido: this.usuario.apellido,
+        correo: this.usuario.correo,
+        rut: this.usuario.rut,
+        telefono: this.usuario.telefono,
+        contrasena: this.usuario.contrasena,
+        contrasena2: this.usuario.contrasena2
+      }
+    }
+
+    this.nCtrl.navigateForward(['/profile'], nExtras);
+
   }
 
 }
